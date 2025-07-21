c14e3bce659776881312b9d189e1532a8b989193
msson1120 <msson2802@gmail.com>
2025-07-21 12:47:18 +0900

    Update app_main_zipupload_fixed (1).py

--- app_main_zipupload_fixed (1).py
+++ app_main_zipupload_fixed (1).py
@@ -20,12 +20,12 @@ if password != '126791':
 st.title("🧾 (주)건화 등기부등본 통합분석기")
 
 # PDF 매뉴얼 토글
-with st.expander("📖 등기부등본 자동정리 프로그램 매뉴얼 보기", expanded=False):
+with st.expander("📖 매뉴얼 보기", expanded=False):
     st.markdown("[매뉴얼 PDF 직접 다운로드](https://raw.githubusercontent.com/msson1120/deunggi-app/main/%EB%93%B1%EA%B8%B0%EB%B6%80%EB%93%B1%EB%B3%B8%EC%9E%90%EB%8F%99%EC%A0%95%EB%A6%AC%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%A8_Manual.pdf)")
     st.components.v1.iframe(
         "https://docs.google.com/gview?url=https://raw.githubusercontent.com/msson1120/deunggi-app/main/%EB%93%B1%EA%B8%B0%EB%B6%80%EB%93%B1%EB%B3%B8%EC%9E%90%EB%8F%99%EC%A0%95%EB%A6%AC%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%A8_Manual.pdf&embedded=true",
         height=600,
-        width="100%"
+        width=1200
     )
 
 st.markdown("""
